03856e79a6185832ee40bd34d63a839277fa008f
Miguel Zavala <miguelzavalac@gmail.com>
2023-11-01 10:40:23 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `papperPlannerWeb`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
